afa211b728c789f4263e4315f68c487c3d6221e7
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-06-09 20:49:51 +0700

    feat: be-xay-dựng-model-movies refs: WA-48

--- src/app.js
+++ src/app.js
@@ -3,13 +3,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import routerAuth from "./routers/auth";
-import routerMovies from "./routers/movies";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/api',routerAuth)
-app.use('/api',routerMovies)
+
 mongoose.connect(`mongodb://127.0.0.1:27017/duan-movies`);
 
 export const viteNodeApp = app;
--- src/routers/movies.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { create, get, getAll } from "../controllers/movies";
-
-const router = express.Router();
-router.get("/movies", getAll);
-router.post("/moviesAdd", create);
-router.get("/movies/:id", get); 
-
-export default router;
