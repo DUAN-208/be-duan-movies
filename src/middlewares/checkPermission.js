9aa2bfa44da7f9a77f993e3d81de1b20e2742622
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2023-06-11 12:03:30 +0700

    feat: be-check-quyền-admin refs;WA-69

---
+++ src/middlewares/checkPermission.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import  User from '../models/auth';
+
+
+export const checkPermission =async (req, res, next) => {
+        try {
+                if(!req.headers.authorization){
+                        return res.json({
+                                message:'Bạn chưa đăng nhập'
+                        })
+                }
+                const token = req.headers.authorization.split(" ")[1]
+                const {id} = jwt.verify(token,'123456')
+                const user = await User.findById(id)
+
+                if(user.role != 'admin'){
+                        return res.json({
+                                message:"Bạn không phải admin"
+                        })
+                }
+                next()
+        } catch (error) {
+                return res.json({ message: error.message });
+
+                
+        }
+}
--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { create, get, remove, update } from "../controllers/movies";
 import { getAll } from "../controllers/movies";
+import { checkPermission } from "../middlewares/checkPermission";
 
 const router = express.Router(); 
-router.post("/movies", create)
+router.post("/movies",checkPermission ,create)
 router.get('/movies',getAll)
-router.patch("/movies/:id", update);
-router.delete("/movies/:id", remove);
+router.patch("/movies/:id",checkPermission, update);
+router.delete("/movies/:id",checkPermission, remove);
 router.get("/movies/:id", get);
 
 
